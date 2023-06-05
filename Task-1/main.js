137966a381bbda689d31ce4290306b4cfe635870
tanisha <tanishamodi12@gmail.com>
2023-06-05 12:49:27 +0530

    task-1 complete

--- Task-1/main.js
+++ Task-1/main.js
@@ -1,9 +1,11 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let img = document.querySelector('.home-img');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+    img.classList.remove('home-img');
 }
 window.onscroll = () => {
     menu.classList.remove('bx-x');
