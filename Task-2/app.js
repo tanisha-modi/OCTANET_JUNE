0ac13e64ad008aac741253305bb2803d661d175b
tanisha <tanishamodi12@gmail.com>
2023-06-15 23:45:22 +0530

    task -2 start

---
+++ Task-2/app.js
@@ -0,0 +1,42 @@
+// selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Function 
+function addTodo(event){
+
+    // prevent form from submitting 
+    event.preventDefault();
+
+    // todoDiv
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // checkmark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // append to list
+    todoList.appendChild(todoDiv);
+
+    //clear input value 
+    todoInput.value = "";
+}
